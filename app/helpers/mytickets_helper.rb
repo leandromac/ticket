7458cefe7248bb0703a7b92ead49d194520c6655
Danilo Resende <danilo.isr@gmail.com>
2015-09-10 22:53:02 -0300

    Add a ultra-simples helper to get ticket status
    TASK Leandro:
    Esse helper está bem bizarro, hehehehe
    Mas a ideia é bem simples, o que falta fazer é trocar os ternarios por
    uns ifs melhores =)

--- app/helpers/mytickets_helper.rb
+++ app/helpers/mytickets_helper.rb
@@ -1,2 +1,9 @@
 module MyticketsHelper
+  def status_label(myticket)
+    (myticket.status == 'waiting') ?
+      'warning' :
+      ((myticket.status == 'active') ?
+        'success' :
+        'default')
+  end
 end
